5587cbf71ad04749799602b1608eec8f01c37c4d
exampleOne <a.vasilievaaa@yandex.ru>
2022-02-17 19:43:15 +0300

    Домашнее задание №7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Задание №1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Задание №2
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
+
+        //Задание №3
+        String fullName3 = "Иванов Семён Семёнович";
+        fullName3 = fullName3.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName3);
     }
 }
